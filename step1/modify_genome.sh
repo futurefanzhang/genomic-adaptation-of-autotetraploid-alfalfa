2b013d96ca4c0d5227141687808a99f9f78f4ed6
futurefanzhang <61524715+futurefanzhang@users.noreply.github.com>
2025-03-26 17:40:39 +0800

    Update modify_genome.sh

--- step1/modify_genome.sh
+++ step1/modify_genome.sh
@@ -8,3 +8,9 @@ python getgaps.py Mpoly.chr3.fa > gaps.gff3
 ##overlapping the Mpoly_MtA17.filtered.coords and gaps.gff3 info
 ##find the breakpoint
 Chr3	.	gap	51900483	51900582	.	.	.	Name=gap9;size=100
+##seperate the Chr3 into Chr3 and Chr7
+echo -e "Chr3\t0\t51900483" | bedtools getfasta -fi Mpoly.chr3.fa -bed - -fo Mpoly.chr7.fa
+sed -i 's/Chr3:0-51900483/Chr7/g' Mpoly.chr7.fa
+echo -e "Chr3\t51900582\t93525394" | bedtools getfasta -fi Mpoly.chr3.fa -bed - -fo Mpoly.chr3_use.fa
+sed -i 's/Chr3:51900582-93525394/Chr3/g' Mpoly.chr3_use.fa
+
