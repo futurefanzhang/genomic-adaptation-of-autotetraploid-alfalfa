6408eb49f92b81d6b68905b1efcb26b44744ac2b
futurefanzhang <61524715+futurefanzhang@users.noreply.github.com>
2025-05-10 16:04:02 +0800

    Create core_gene_in_ZM4V2.sh

---
+++ step2.1/core_gene_in_ZM4V2.sh
@@ -0,0 +1,14 @@
+#extract information in orthofinder
+cut -f1,13 /data/home/zhangfan/compare_genomoe_medicago/genespace_new/orthofinder/Results_May09/Orthogroups/Orthogroups.tsv > ZM4_V2_gene_family_detail.txt ##提取目标基因组的信息
+python change_gene_family.py ZM4_V2_gene_family_detail.txt ZM4_V2_gene_family_detail_change_format.txt ##将每行多基因拆分为每行一个基因的格式
+##follow steps in R
+
+mydata=read.table("pan_gene_info.txt",row.names = 1,head = T,sep = "\t")
+gene_detail=read.table("ZM4_V2_gene_family_detail_change_format.txt",head = T,sep = "\t")
+mydata$Orthogroup=rownames(mydata)
+mydata_use=subset(mydata, select = c(Orthogroup,sum))  ##select: Orthogroup,sum
+library(dplyr)
+all_gene_info <- gene_detail %>% left_join(mydata_use, by=c('Orthogroup'='Orthogroup'))
+write.table(all_gene_info,"all_gene_orthogroup_pan_gene_info.txt",row.names = F,col.names = T,sep = "\t",quote = F)
+counts <- all_gene_info %>% count(sum)
+counts  ##统计每一类基因的占比，我的结果核心基因占比30.59%， 私有基因11.44%
