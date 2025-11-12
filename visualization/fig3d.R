7411a1b79870d3cf52992a1ceb6ca4ebe6dad49c
futurefanzhang <61524715+futurefanzhang@users.noreply.github.com>
2025-11-12 19:19:27 +0800

    Update fig3d.R

--- visualization/fig3d.R
+++ visualization/fig3d.R
@@ -4,8 +4,8 @@ colnames(mydata)=c("quary","hit","blast_pvalue","ka","ks")
 mydata=mydata[mydata$ks>0,]
 mydata=mydata[mydata$ka>0,]
 mydata$kavsks=mydata$ka/mydata$ks  #generate ka/ks
-mydata_separated <- mydata %>%
-  separate(quary, into = c("gene", "transcript"), sep = ".t")
+library(tidyverse)
+mydata_separated <- mydata %>% separate(quary, into = c("gene", "transcript"), sep = ".t")
 ##use the mean ka/ks of multiple paired gene as the final ka/ks
 mydata_result <- mydata_separated %>%
   group_by(gene) %>%
@@ -26,6 +26,7 @@ merged_df <- merged_df %>%
                        
 merged_df$Type <- factor(merged_df$Type,levels = c("One copy","Two copies","Three copies","Four copies"),ordered = TRUE) 
 merged_df$sum <- factor(merged_df$sum,levels = c("Private","Dispensable","Softcore","Core"),ordered = TRUE)
+
 p1=ggplot(data=merged_df,aes(x = sum, y = log_kavsks, fill = Type)) +
   scale_fill_manual(breaks=c("One copy","Two copies","Three copies","Four copies"),
                     values=c("#ABB3CA","#FEC87E","#A2CC00","#FF9477"))+  ##不同类型基因对应的颜色
@@ -39,4 +40,15 @@ p1=ggplot(data=merged_df,aes(x = sum, y = log_kavsks, fill = Type)) +
     axis.text.y = element_text(size=14),
     axis.line = element_line(size=0.5, colour = 'black'), #轴线
     plot.margin = margin(t = 0.5, r = 0, b = 0.5, l = 0.5, unit = "cm"))
-ggsave(p1,filename = "box_core_gene_copy_number_kaks.pdf",width = 7,height = 6,dpi=300)
+
+ggsave(p1,filename = "box_core_gene_copy_number_kaks_v2.pdf",width = 7,height = 6,dpi=300)
+library(dplyr)
+
+# 计算每个组的 N 值和标签位置
+summary_df <- merged_df %>%
+  group_by(sum, Type) %>%  # 根据箱线图的分组变量进行分组
+  summarise(
+    n = n(),
+  ) %>%
+  ungroup()
+summary_df
